cae881b345e541d3bfb5afb729ac476d5fb0432e
mlezhnin <michaellezh@gmail.com>
2023-12-18 00:16:33 -0500

    Testing more responsive fonts

---
+++ source.js
@@ -0,0 +1,15 @@
+// function useVwOrVh() {
+//     var vh = window.innerHeight * 0.01;
+//     var vw = window.innerWidth * 0.01;
+//     var vwOrVh = "vh"
+//     if(vw > vh) {vwOrVh = "vw"}
+//     document.documentElement.style.fontSize = "1.5" + vwOrVh;
+// }
+
+// // Run on load
+// useVwOrVh();
+
+// // Run on window resize
+// window.onresize = useVwOrVh;
+
+console.log("poop")
