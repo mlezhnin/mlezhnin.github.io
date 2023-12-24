91535ff6b7edd17b42f04dffc54a5a622cad0009
mlezhnin <michaellezh@gmail.com>
2023-12-24 16:57:17 -0500

    In progress color picker tutorial

---
+++ posts/colorPicker/picker.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const imageUploader = document.getElementById('imageUploader');
+    const imageCanvas = document.getElementById('imageCanvas');
+    const ctx = imageCanvas.getContext('2d');
+    const selectedColor = document.getElementById('selectedColor');
+
+    imageUploader.addEventListener('change', function (e) {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+
+        reader.onload = function (event) {
+            const img = new Image();
+            img.onload = function () {
+                imageCanvas.width = img.width;
+                imageCanvas.height = img.height;
+                ctx.drawImage(img, 0, 0);
+            }
+            img.src = event.target.result;
+        }
+
+        reader.readAsDataURL(file);
+    });
+
+    imageCanvas.addEventListener('mousemove', async function (e) {
+        const x = e.offsetX;
+        const y = e.offsetY;
+        const pixel = ctx.getImageData(x, y, 1, 1);
+        const bitmap = await createImageBitmap(ctx.getImageData(x-2, y-2, 5, 5))
+        magnify(bitmap, e.x, e.y);
+        const data = pixel.data;
+        const rgba = 'rgba(' + data[0] + ', ' + data[1] + ', ' + data[2] + ', ' + (data[3] / 255) + ')';
+        selectedColor.innerText = rgba;
+    });
+
+});
+
+function magnify(bitmap, x, y) {
+    const magnifierLens = document.getElementById('magnifierLens');
+    const ctx = magnifierLens.getContext('2d');
+
+    magnifierLens.style.position='absolute'
+    magnifierLens.style.left = x - 49 + 'px';
+    magnifierLens.style.top = y - 49 + 'px';
+    
+    ctx.imageSmoothingEnabled = false;
+    ctx.drawImage(bitmap, 0, 0, magnifierLens.width, magnifierLens.height);
+}
